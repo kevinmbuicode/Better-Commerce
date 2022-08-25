2e186d698fea2b25d894eb9c96844054fe0b3407
kevinmbuicode <kevinmushan@gmail.com>
2022-08-25 14:40:09 +0300

    Finished NavBar, Made resposive

--- better-commerce/src/App.js
+++ better-commerce/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import ProductInfo from './components/ProductInfo';
 
 
 function App() {
   return (
     <>
-      App
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path='/products/:id' element={<ProductInfo/>}/>
+      </Routes>
     </>
   );
 }
---
+++ better-commerce/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from './Carousel'
+
+const Home = () => {
+  return (
+    <>
+        <Carousel/>
+    </>
+  )
+}
+
+export default Home
